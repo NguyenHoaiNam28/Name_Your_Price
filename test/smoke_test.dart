2631d8f720ab449505e8dd9e37243ee1e84076e1
Nguyen Hoai Nam <namqkth@gmail.com>
2022-06-21 22:59:46 +0700

    update test

---
+++ test/smoke_test.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:name_your_price/main.dart';
+
+void main() {
+  testWidgets('smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    
+  });
+}
